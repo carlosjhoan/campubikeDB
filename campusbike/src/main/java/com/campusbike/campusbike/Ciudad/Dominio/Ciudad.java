38af0119c002232d564e679798113950ca21e8b3
carlosjhoan <carlosjhoanaguilar@gmail.com>
2024-09-19 22:29:30 -0500

    Alguna modificación sencilla

--- campusbike/src/main/java/com/campusbike/campusbike/Ciudad/Dominio/Ciudad.java
+++ campusbike/src/main/java/com/campusbike/campusbike/Ciudad/Dominio/Ciudad.java
@@ -5,16 +5,14 @@ package com.campusbike.campusbike.Ciudad.Dominio;
 
 import com.campusbike.campusbike.Pais.Dominio.Pais;
 
-import jakarta.persistence.CascadeType;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
